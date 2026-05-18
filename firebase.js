28c3b0309db19c0d6fc71bac4b263910abb2a580
myfilessharedin-wq <myfilessharedin@gmail.com>
2026-05-18 20:11:58 +0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -5,12 +5,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyBxYBR2OWosExkGfynF9cF38BFF3Onidyk",
+  authDomain: "goals-ca808.firebaseapp.com",
+  projectId: "goals-ca808",
+  storageBucket: "goals-ca808.firebasestorage.app",
+  messagingSenderId: "667647333017",
+  appId: "1:667647333017:web:d4966e0613f7b71a525e08",
+  measurementId: "G-2FW7B253N7"
 };
 
 const app = initializeApp(firebaseConfig);
