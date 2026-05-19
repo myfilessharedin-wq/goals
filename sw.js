bd95dcc5fd9dddd31e721f27f641449e69e7d4b4
myfilessharedin-wq <myfilessharedin@gmail.com>
2026-05-19 11:55:16 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "goals-cache-v1";
+const CACHE_NAME = "heart-rpg-cache-v2";
 
 const urlsToCache = [
   "./",
@@ -10,7 +10,16 @@ const urlsToCache = [
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
